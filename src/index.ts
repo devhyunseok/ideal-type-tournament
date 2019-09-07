bfc788e4cc0863da2b7c208074426911d9bccb4c
김현석 <devhyunseok@gmail.com>
2019-09-07 18:41:22 +0900

    이전으로 돌아가기 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -8,41 +8,89 @@ const btnLeft = <HTMLButtonElement>document.getElementById("btn-left-img");
 const btnRight = <HTMLButtonElement>document.getElementById("btn-right-img");
 const curRound = <HTMLSpanElement>document.getElementById("current-round");
 const historyListEl = <HTMLUListElement>document.getElementById("history-list");
+const btnBackEl = <HTMLButtonElement>document.getElementById("btn-back");
 
-let roundCount = 0;
-let history = [];
-let round : Stage[] = [];
+let roundCount = 0; // 현재 라운드
+let history: Array<Stage[]> = [];
+let round : Array<Stage> = [];
 
 const main = () => {
   // TODO: 나중에 해제 해야함.
   btnLeft.addEventListener('click', leftImageClick);
   btnRight.addEventListener('click', rightImageClick);
   
-  const shuffle = shuffleList(comicList);
+  // 뒤로가기
+  btnBackEl.addEventListener('click', cancelClick);
 
-  initNewRound(shuffle);
+  //const shuffle = shuffleList(comicList);
+
+  initNewRound(comicList);
   
   console.log(round);
   console.log(comicList);
-  console.log(shuffle);
+  // console.log(shuffle);
   
   nowBattle(round[roundCount]);
 };
+
+// 취소 버튼
+const cancelClick = () => {
+  // 취소할 내역이 없음.
+  if(roundCount <= 0 && history.length <= 0) {
+    return; 
+  }
+
+  // 현재 라운드에서 하나라도 선택되었을 경우.
+  if(roundCount > 0) {
+    roundCount--;
+
+    // 이전 선택 사항 취소.
+    round[roundCount].getLeft().selected = false;
+    round[roundCount].getRight().selected = false;
+
+    nowBattle(round[roundCount]);
+    return;
+  }
+
+  // 히스토리 이전 내용을 가져옴
+  if(roundCount <= 0 && history.length > 0) {
+    const prevRound = history.pop();
+
+    console.log(prevRound);
+
+    round = prevRound && prevRound.length > 0 ? prevRound : [];
+    roundCount = round.length - 1;
+
+     // 이전 선택 사항 취소.
+    round[roundCount].getLeft().selected = false;
+    round[roundCount].getRight().selected = false;
+ 
+    // 현재 라운드 재 표기
+    curRound.textContent = String(round.length * 2);
+
+    nowBattle(round[roundCount]);
+  }
+}
+
+
+
+
 // 새로운 라운드를 생성
 const initNewRound = (list: Comic[]) => {
   round = []; // 초기화
   roundCount = 0;
+  const shuffledList = shuffleList(list);
 
-  for (let i = 0; i < list.length; i+=2) {
+  for (let i = 0; i < shuffledList.length; i+=2) {
     console.log(i);
-    round.push(new Stage(list[i], list[i+1]))
+    round.push(new Stage(shuffledList[i], shuffledList[i+1]))
   }
 
   curRound.textContent = String(round.length * 2);
 
   // li 추가
   const liEl = document.createElement("li");
-  list.forEach(item => liEl.appendChild(document.createTextNode(`${item.getName()}, `)))
+  shuffledList.forEach(item => liEl.appendChild(document.createTextNode(`${item.getName()}, `)))
   historyListEl.appendChild(liEl);
 };
 
